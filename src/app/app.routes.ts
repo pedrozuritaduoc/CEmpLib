1ad84b848154c001b37c04f8b844cfd28efa74e1
Pedro Zurita <pe.zurita@duocuc.cl>
2024-10-21 22:46:49 -0300

    v3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { Articulo1Component } from './pages/articulo1/articulo1.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
 
-    { path: 'articulo1-component', component: Articulo1Component }
+    { path: 'articulo1-component', component: Articulo1Component },
+    { path: 'contacto-component', component: ContactoComponent }
 ];
